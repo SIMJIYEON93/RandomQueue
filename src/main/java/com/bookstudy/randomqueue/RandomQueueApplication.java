f3c4d8ee395ab287fde54429a7150a18f289b005
SIMJIYEON93 <930515s@gmail.com>
2024-10-01 15:56:30 +0900

    Initial commit

---
+++ src/main/java/com/bookstudy/randomqueue/RandomQueueApplication.java
@@ -0,0 +1,13 @@
+package com.bookstudy.randomqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomQueueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookstudy/randomqueue/RandomQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstudy.randomqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
